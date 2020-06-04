4d6746c5aae4936ee8478d699b15ccffc4be7a26
AdamK <adamkoziel@outlook.com>
2020-06-04 11:52:01 +0100

    lab complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,39 @@
 require_relative('../models/student.rb')
+require_relative('../models/house.rb')
 require('pry')
+
+gryffindor = House.new({
+  "name" => "Gryffindor"
+})
+
+ravenclaw = House.new({
+  "name" => "Ravenclaw"
+})
+
+hufflepuff = House.new({
+  "name" => "Hufflepuff"
+})
+
+slytherin = House.new({
+  "name" => "Slytherin"
+})
+
+gryffindor.save()
+slytherin.save()
+ravenclaw.save()
+hufflepuff.save()
 
 student1 = Student.new({
   "first_name" => "Harry",
   "last_name" => "Potter",
-  "house" => "Gryffindor",
+  "house_id" => 1,
   "age" => 11
 })
 
 student2 = Student.new({
   "first_name" => "Ron",
   "last_name" => "Weasley",
-  "house" => "Gryffindor",
+  "house_id" => 1,
   "age" => 12
 })
 
--- db/students.sql
+++ db/students.sql
@@ -1,9 +1,15 @@
 DROP TABLE students;
+DROP TABLE houses;
+
+CREATE TABLE houses (
+  id SERIAL primary key,
+  name VARCHAR(255)
+);
 
 CREATE TABLE students (
   id SERIAL primary key,
   first_name VARCHAR(255),
   last_name VARCHAR(255),
-  house VARCHAR(255),
+  house_id INT REFERENCES houses(id) ON DELETE CASCADE,
   age INT
 );
